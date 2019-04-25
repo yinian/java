de44f19354a479638dc9d122c807d22a5be6cb2a
viagra <1356971566@qq.com>
2019-04-25 14:29:33 +0800

    fixed

--- spring-project/spring-cloud-project/imooc-ad-spring-cloud/imooc-ad-service/ad-search/README.md
+++ spring-project/spring-cloud-project/imooc-ad-spring-cloud/imooc-ad-service/ad-search/README.md
@@ -2,9 +2,23 @@
 
 `@IgnoreResponseAdvice`: 屏蔽统一响应请求
 
-基于Feign实现微服务访问：声明式的web服务客户端，比ribbon访问更简单。
-
+client包：基于Feign实现微服务访问：声明式的web服务客户端，比ribbon访问更简单。
 `AdPlan` 和`AdPlanGetRequest` 这两个是个演示案例，服务中具体用不到。
 使用了`Hystrix` 断路器来防止雪崩。
 
+index包：实现各种数据索引功能，具体详情请查看类。
+        IndexFileLoader类：全量索引的具体实现
+service包：BinlogServiceTest类：对mysql-binlog-connector-java 开源组件的测试用例。
+mysql/dto包：按照resources/template.json 格式，对mysql-binlog-connector-java进行定制化开发.
+sender包：增量索引
+search包：广告检索
+
+    1.DataTable: index缓存的实现
+handler包：实现增量索引的一个Handler.
+
+
+
+
+
 作用：通过Feign客户端调用微服务，通过Hystrix实现服务降级。
+
--- spring-project/spring-cloud-project/imooc-ad-spring-cloud/imooc-ad-service/ad-search/src/main/java/com/imooc/ad/controller/SearchController.java
+++ spring-project/spring-cloud-project/imooc-ad-spring-cloud/imooc-ad-service/ad-search/src/main/java/com/imooc/ad/controller/SearchController.java
@@ -5,6 +5,9 @@ import com.imooc.ad.annotation.IgnoreResponseAdvice;
 import com.imooc.ad.client.SponsorClient;
 import com.imooc.ad.client.vo.AdPlan;
 import com.imooc.ad.client.vo.AdPlanGetRequest;
+import com.imooc.ad.search.ISearch;
+import com.imooc.ad.search.vo.SearchRequest;
+import com.imooc.ad.search.vo.SearchResponse;
 import com.imooc.ad.vo.CommonResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -25,21 +28,56 @@ import java.util.List;
 @RestController
 public class SearchController {
 
+	private final ISearch search;
+
 	private final RestTemplate restTemplate;
 
 	private final SponsorClient sponsorClient;
 
 	@Autowired
-	public SearchController(RestTemplate restTemplate, SponsorClient sponsorClient){
+	public SearchController(RestTemplate restTemplate,
+	                        SponsorClient sponsorClient, ISearch search) {
 		this.restTemplate = restTemplate;
 		this.sponsorClient = sponsorClient;
+		this.search = search;
 	}
 
+	@PostMapping("/fetchAds")
+	public SearchResponse fetchAds(@RequestBody SearchRequest request) {
+
+		log.info("ad-search: fetchAds -> {}",
+				JSON.toJSONString(request));
+		return search.fetchAds(request);
+	}
+
+
+
+	@SuppressWarnings("all")
+	@IgnoreResponseAdvice
+	@PostMapping("/getAdPlansByRibbon")
+	public CommonResponse<List<AdPlan>> getAdPlansByRebbon(
+			@RequestBody AdPlanGetRequest request
+	) {
+		log.info("ad-search: getAdPlansByRibbon -> {}",
+				JSON.toJSONString(request));
+		return restTemplate.postForEntity(
+				"http://eureka-client-ad-sponsor/ad-sponsor/get/adPlan",
+				request,
+				CommonResponse.class
+		).getBody();
+	}
+
+	@IgnoreResponseAdvice
+	@PostMapping("/getAdPlans")
 	public CommonResponse<List<AdPlan>> getAdPlans(
-			@RequestBody AdPlanGetRequest request){
-		log.info("ad-search: getAdPlansByRibbon -> {}", JSON.toJSONString(request));
+			@RequestBody AdPlanGetRequest request
+	) {
+		log.info("ad-search: getAdPlans -> {}",
+				JSON.toJSONString(request));
 		return sponsorClient.getAdPlans(request);
 	}
+
+
 
 
 	@IgnoreResponseAdvice
