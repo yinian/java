c2b5eaa9b14eee7474a8770bf09666b8c7476d83
viagra <1356971566@qq.com>
2019-04-25 14:36:54 +0800

    对应表的实体类

---
+++ spring-project/spring-cloud-project/imooc-ad-spring-cloud/imooc-ad-service/ad-common/src/main/java/com/imooc/ad/dump/DConstant.java
@@ -0,0 +1,19 @@
+package com.imooc.ad.dump;
+
+
+/**
+ * 全量导出的文件名和路径
+ */
+public class DConstant {
+
+    public static final String DATA_ROOT_DIR = "F:\\java_system\\Spring_Family_Bucket\\Spring_Project\\Project-02\\imooc\\imooc-ad-spring-cloud\\imooc-ad-service\\ad-common\\src\\main\\resources\\mysql_data";
+
+    // 各个表数据的存储文件名
+    public static final String AD_PLAN = "ad_plan.data";
+    public static final String AD_UNIT = "ad_unit.data";
+    public static final String AD_CREATIVE = "ad_creative.data";
+    public static final String AD_CREATIVE_UNIT = "ad_creative_unit.data";
+    public static final String AD_UNIT_IT = "ad_unit_it.data";
+    public static final String AD_UNIT_DISTRICT = "ad_unit_district.data";
+    public static final String AD_UNIT_KEYWORD = "ad_unit_keyword.data";
+}
