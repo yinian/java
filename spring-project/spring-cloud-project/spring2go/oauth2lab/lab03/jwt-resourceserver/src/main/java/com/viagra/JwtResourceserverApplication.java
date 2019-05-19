1bf038e56cd4d0de8f87d9be5fbfbda0ef9028e3
viagra <1356971566@qq.com>
2019-05-19 08:25:13 +0800

    push

---
+++ spring-project/spring-cloud-project/spring2go/oauth2lab/lab03/jwt-resourceserver/src/main/java/com/viagra/JwtResourceserverApplication.java
@@ -0,0 +1,13 @@
+package com.viagra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtResourceserverApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JwtResourceserverApplication.class, args);
+    }
+
+}
---
+++ spring-project/spring-cloud-project/spring2go/oauth2lab/lab03/jwt-resourceserver/src/main/java/com/viagra/api/UserInfo.java
@@ -0,0 +1,17 @@
+package com.viagra.api;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class UserInfo {
+
+    private String name;
+
+    private String email;
+
+
+}
