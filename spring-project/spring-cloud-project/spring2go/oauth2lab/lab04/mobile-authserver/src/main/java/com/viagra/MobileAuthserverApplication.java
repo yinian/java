36b4316cba8e4d562dc06ff95cfb658cb9a35476
viagra <1356971566@qq.com>
2019-05-19 08:28:37 +0800

    push

---
+++ spring-project/spring-cloud-project/spring2go/oauth2lab/lab04/mobile-authserver/src/main/java/com/viagra/MobileAuthserverApplication.java
@@ -0,0 +1,13 @@
+package com.viagra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MobileAuthserverApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MobileAuthserverApplication.class, args);
+    }
+
+}
---
+++ spring-project/spring-cloud-project/spring2go/oauth2lab/lab04/mobile-authserver/src/main/java/com/viagra/UserInfo.java
@@ -0,0 +1,15 @@
+package com.viagra;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class UserInfo {
+
+    private String name;
+
+    private String email;
+
+
+}
