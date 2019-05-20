8a0e5687b6342607ba1161259a95c6c3c75c425a
viagra <1356971566@qq.com>
2019-05-20 22:40:58 +0800

    跨站链请求伪造客户端

---
+++ spring-project/spring-cloud-project/spring2go/oauth2lab/lab07/state-param-security/state-client/src/main/java/com/viagra/ClientStateApplication.java
@@ -0,0 +1,12 @@
+package com.viagra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientStateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClientStateApplication.class, args);
+	}
+}
