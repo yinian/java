3023fb952c6f8dcb8d4ed2e5f6474f89af6942ca
viagra <1356971566@qq.com>
2019-08-03 12:56:56 +0800

    第8章 Protobuf 编解码开发

---
+++ netty/cookbook/netty-UserGuide/src/main/java/com/viagra/chapter08/protobuf/README.md
@@ -0,0 +1,4 @@
+## protobuf生成命令
+
+protoc SubscribeReq.proto --java_out=D:\java\netty\cookbook\netty-UserGuide\src\main\java\com\viagra\chapter08\protobuf --proto_path=D:\java\netty\cookbook\netty-UserGuide\src\main\java\com\viagra\chapter08\protobuf
+protoc SubscribeResp.proto --java_out=D:\java\netty\cookbook\netty-UserGuide\src\main\java\com\viagra\chapter08\protobuf --proto_path=D:\java\netty\cookbook\netty-UserGuide\src\main\java\com\viagra\chapter08\protobuf
---
+++ netty/cookbook/netty-UserGuide/src/main/java/com/viagra/chapter08/protobuf/TestSubscribeReqProto.java
@@ -0,0 +1,52 @@
+package com.viagra.chapter08.protobuf;
+
+import com.google.protobuf.InvalidProtocolBufferException;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @Auther: viagra
+ * @Date: 2019/8/1 14:54
+ * @Description:
+ */
+public class TestSubscribeReqProto {
+
+    private static byte[] encode(SubscribeReqProto.SubcribeReq req) {
+        return req.toByteArray();
+    }
+
+    private static SubscribeReqProto.SubcribeReq decode(byte[] body)
+            throws InvalidProtocolBufferException {
+        return SubscribeReqProto.SubcribeReq.parseFrom(body);
+    }
+
+    private static SubscribeReqProto.SubcribeReq createSubscribeReq() {
+        SubscribeReqProto.SubcribeReq.Builder builder = SubscribeReqProto.SubcribeReq
+                .newBuilder();
+        builder.setSubReqID(1);
+        builder.setUserName("Lilinfeng");
+        builder.setProductName("Netty Book");
+        List<String> address = new ArrayList<>();
+        address.add("NanJing YuHuaTai");
+        address.add("BeiJing LiuLiChang");
+        address.add("ShenZhen HongShuLin");
+        builder.addAllAddress(address);
+        return builder.build();
+    }
+
+    /**
+     * @param args
+     * @throws InvalidProtocolBufferException
+     */
+    public static void main(String[] args)
+            throws InvalidProtocolBufferException {
+        SubscribeReqProto.SubcribeReq req = createSubscribeReq();
+        System.out.println("Before encode : " + req.toString());
+        SubscribeReqProto.SubcribeReq req2 = decode(encode(req));
+        System.out.println("After decode : " + req.toString());
+        System.out.println("Assert equal : --> " + req2.equals(req));
+
+    }
+
+}
