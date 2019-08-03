0ccd9a949a4e8a322f4c73249fcf6b497e0d8426
viagra <1356971566@qq.com>
2019-08-03 12:41:02 +0800

    第3章 netty入门应用

---
+++ netty/cookbook/netty-UserGuide/README.md
@@ -0,0 +1,17 @@
+## 源代码地址
+
+https://github.com/wuyinxian124/nettybook2
+
+##　各章节目录
+    
+#### chapter05
+ 
+* com.viagra.chapter05.delimiter：使用`DelimiterLengthFrameDecoder`进行开发 
+* com.viagra.chapter05.FixedLen ：使用`FixedLengthFrameDecoder` 进行开发
+
+#### chapter06
+
+* com.viagra.chapter06.MessagePack 使用`MessagePack` 进行开发，未解决TCP粘包/拆包问题
+* com.viagra.chapter06.updatedMessagePack 使用`MessagePack` 进行开发，解决了TCP粘包/拆包问题
+
+修改了EchoServer.java,EchoClient.java即可
---
+++ netty/cookbook/netty-UserGuide/src/main/java/com/viagra/chapter03/TimeClient.java
@@ -0,0 +1,60 @@
+package com.viagra.chapter03;
+
+import io.netty.bootstrap.Bootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
+
+/**
+ * @Auther: viagra
+ * @Date: 2019/7/29 17:30
+ * @Description: netty 客户端
+ */
+public class TimeClient {
+
+    public void connect(int port, String host)  throws Exception {
+        // 配置客户端NIO 线程组
+        EventLoopGroup group = new NioEventLoopGroup();
+
+        try {
+            Bootstrap b = new Bootstrap();
+            b.group(group).channel(NioSocketChannel.class)
+                    .option(ChannelOption.TCP_NODELAY,true)
+                    .handler(new ChannelInitializer<SocketChannel>() {
+
+                        @Override
+                        protected void initChannel(SocketChannel socketChannel) throws Exception {
+                            socketChannel.pipeline().addLast(new TimeClientHandler());
+                        }
+                    });
+
+            // 发起异步连接操作
+            ChannelFuture f = b.connect(host, port).sync();
+
+            // 当代客户端链路关闭
+            f.channel().closeFuture().sync();
+        } finally {
+            // 优雅退出，释放NIO线程组
+            group.shutdownGracefully();
+        }
+    }
+    /**
+     * @param args
+     * @throws Exception
+     */
+    public static void main(String[] args) throws Exception {
+        int port = 8080;
+        if (args != null && args.length > 0) {
+            try {
+                port = Integer.valueOf(args[0]);
+            } catch (NumberFormatException e) {
+                // 采用默认值
+            }
+        }
+        new TimeClient().connect(port, "127.0.0.1");
+    }
+}
