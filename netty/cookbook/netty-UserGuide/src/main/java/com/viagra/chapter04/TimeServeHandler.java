7e0d484b9893f15819d66da9e98a0921eae7001f
viagra <1356971566@qq.com>
2019-08-03 12:42:29 +0800

    第4章 TCP粘包/拆包问题的解决之道

---
+++ netty/cookbook/netty-UserGuide/src/main/java/com/viagra/chapter04/TimeServeHandler.java
@@ -0,0 +1,34 @@
+package com.viagra.chapter04;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.ChannelHandlerAdapter;
+import io.netty.channel.ChannelHandlerContext;
+
+/**
+ * @Auther: viagra
+ * @Date: 2019/7/29 16:39
+ * @Description:
+ */
+public class TimeServeHandler extends ChannelHandlerAdapter {
+
+    private int counter;
+
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg)
+            throws Exception {
+        String body = (String) msg;
+        System.out.println("The time server receive order : " + body
+                + " ; the counter is : " + ++counter);
+        String currentTime = "QUERY TIME ORDER".equalsIgnoreCase(body) ? new java.util.Date(
+                System.currentTimeMillis()).toString() : "BAD ORDER";
+        currentTime = currentTime + System.getProperty("line.separator");
+        ByteBuf resp = Unpooled.copiedBuffer(currentTime.getBytes());
+        ctx.writeAndFlush(resp);
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) {
+        ctx.close();
+    }
+}
