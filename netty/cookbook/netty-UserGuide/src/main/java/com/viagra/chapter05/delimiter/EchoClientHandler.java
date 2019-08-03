f450e03283070c8a8ebb5df825704888e0fb2191
viagra <1356971566@qq.com>
2019-08-03 12:44:51 +0800

    第5章 定长编码器 DelimiterBasedFrameDecoder 应用开发

---
+++ netty/cookbook/netty-UserGuide/src/main/java/com/viagra/chapter05/delimiter/EchoClientHandler.java
@@ -0,0 +1,50 @@
+package com.viagra.chapter05.delimiter;
+
+import io.netty.buffer.Unpooled;
+import io.netty.channel.ChannelHandlerAdapter;
+import io.netty.channel.ChannelHandlerContext;
+/**
+ * @Auther: viagra
+ * @Date: 2019/7/30 07:20
+ * @Description:
+ */
+public class EchoClientHandler extends ChannelHandlerAdapter {
+
+    private int counter;
+
+    static final String ECHO_REQ = "Hi, Lilinfeng. Welcome to Netty.$_";
+
+    /**
+     * Creates a client-side handler.
+     */
+    public EchoClientHandler() {
+    }
+
+    @Override
+    public void channelActive(ChannelHandlerContext ctx) {
+        // ByteBuf buf = UnpooledByteBufAllocator.DEFAULT.buffer(ECHO_REQ
+        // .getBytes().length);
+        // buf.writeBytes(ECHO_REQ.getBytes());
+        for (int i = 0; i < 10; i++) {
+            ctx.writeAndFlush(Unpooled.copiedBuffer(ECHO_REQ.getBytes()));
+        }
+    }
+
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg)
+            throws Exception {
+        System.out.println("This is " + ++counter + " times receive server : ["
+                + msg + "]");
+    }
+
+    @Override
+    public void channelReadComplete(ChannelHandlerContext ctx) throws Exception {
+        ctx.flush();
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) {
+        cause.printStackTrace();
+        ctx.close();
+    }
+}
---
+++ netty/cookbook/netty-UserGuide/src/main/java/com/viagra/chapter05/delimiter/EchoServerHandler.java
@@ -0,0 +1,34 @@
+package com.viagra.chapter05.delimiter;
+
+
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.ChannelHandler.Sharable;
+import io.netty.channel.ChannelHandlerAdapter;
+import io.netty.channel.ChannelHandlerContext;
+/**
+ * @Auther: viagra
+ * @Date: 2019/7/30 07:17
+ * @Description:
+ */
+public class EchoServerHandler extends ChannelHandlerAdapter {
+
+    int counter = 0;
+
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg)
+            throws Exception {
+        String body = (String) msg;
+        System.out.println("This is " + ++counter + " times receive client : ["
+                + body + "]");
+        body += "$_";
+        ByteBuf echo = Unpooled.copiedBuffer(body.getBytes());
+        ctx.writeAndFlush(echo);
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) {
+        cause.printStackTrace();
+        ctx.close();// 发生异常，关闭链路
+    }
+}
